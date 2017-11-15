31677d7ffb5e965b1222445edc3930f85b5bb131
Joe Kerns <joe@168tickets.com>
2017-11-15 15:04:17 -0500

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,51 @@
 // entry -> output
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 //console.log(path.join(__dirname,'public'));
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname,'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }
-  ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname,'public'),
-    historyApiFallback: true // this forces the page to stay on the index.html so that react-router can do it's thing  
-  }
-};
+module.exports = (env) => {
+  const isProduction = env == 'production';
+  const cssExtract = new ExtractTextPlugin('styles.css');
 
-// loader
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname,'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: cssExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }
+    ]},
+    plugins: [
+      cssExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname,'public'),
+      historyApiFallback: true // this forces the page to stay on the index.html so that react-router can do it's thing  
+    }
+  }
+}
