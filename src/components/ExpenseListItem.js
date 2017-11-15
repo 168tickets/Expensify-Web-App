4b0b14d18997b1f1065118fd5797508c9e771cd7
Joe Kerns <joe@168tickets.com>
2017-11-15 15:59:58 -0500

    Formatting for amount and created at

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 // const removeExpenseItem = () => {  // this is not need since we use the actions below 
 //   console.log('testing');
@@ -9,7 +11,11 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => {
   return  (  // spread all the props and dispatch so we have access to them...
       <div>
         <h3><Link to={`/edit/${id}`}>{description}</Link></h3> {/* use the `${something}` to inject a variable into a string */}
-        <p>{amount} - {createdAt}</p>
+        <p>
+        {numeral(amount / 100).format('$0,0.00')} 
+        - 
+        {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
         { /* <button onClick={removeExpenseItem}>Remove</button> <-- my lame attempt */ }
       </div>
   );
